aa92518b1eb8c2fb86ef981e8418c1a3ba1c7d16
medcharfi96 <1389@holbertonschool.com>
2020-04-28 18:12:09 +0200

    pep fix

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number > 0:
+        print("{:d}".format(abs(number % 10)), end="")
+    else:
+        print("{:d}".format(abs(number % -10)), end="")
+    return (abs(number) % 10)

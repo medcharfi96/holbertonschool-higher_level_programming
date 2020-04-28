105996a0e8462b6dade02ec2b38873e66676e51e
medcharfi96 <1389@holbertonschool.com>
2020-04-28 16:07:58 +0200

    pep fix

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number < 0:
+    x = (number % 10) * -1
+else:
+    x = number % 10
+print("Last digit of {:d} is {:d} ".format(number, x), end="")
+if x == 0:
+    print("and is 0")
+elif x < 5:
+    print("and is less than 6 and not 0")
+else:
+    print("and is greater than 5")

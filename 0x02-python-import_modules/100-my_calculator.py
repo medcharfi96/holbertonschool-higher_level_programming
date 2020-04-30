08b873c4e36fa261e2888078a811895d8d97dfeb
medcharfi96 <1389@holbertonschool.com>
2020-04-30 22:28:56 +0200

    sahha chribtek

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+import sys
+if __name__ == "__main__":
+    if len(sys.argv) == 4:
+        if (sys.argv[2] != "+" and sys.argv[2] != "*" and sys.argv[2] != "-" and sys.argv[2] != "/"):
+            print("Unknown operator. Available operators: +, -, * and /")
+            sys.exit(1)
+        elif sys.argv[2] == "*":
+            print("{:d} + {:d} = {:d}".format(int(sys.argv[1]), int(sys.argv[3]), mul(int(sys.argv[1]), int(sys.argv[3]))))
+        elif sys.argv[2] == "+":
+            print("{:d} + {:d} = {:d}".format(int(sys.argv[1]), int(sys.argv[3]), add(int(sys.argv[1]), int(sys.argv[3]))))
+        elif sys.argv[2] == "-":
+            print("{:d} - {:d} = {:d}".format(int(sys.argv[1]), int(sys.argv[3]), sub(int(sys.argv[1]), int(sys.argv[3]))))
+        elif sys.argv[2] == "/":
+            print("{:d} / {:d} = {:d}".format(int(sys.argv[1]), int(sys.argv[3]), div(int(sys.argv[1]), int(sys.argv[3]))))
+    else:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)

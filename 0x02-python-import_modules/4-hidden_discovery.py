41af2255ccf9f753f89d1ea6f2ccaa601e86afd9
medcharfi96 <1389@holbertonschool.com>
2020-04-30 18:27:52 +0200

    comm

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import hidden_4
 if __name__ == "__main__":
-    for i in range(0, len(dir(hidden_4))):
-        if i[0] != '_' and i[1] != '_':
-            print("{:d}\n".format(i))
+    import hidden_4
+    for tes in dir(hidden_4):
+        if tes[0:2] != "__":
+            print("{}".format(tes))

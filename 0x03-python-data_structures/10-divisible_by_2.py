6fac1418bc353e9e6c410f5d8a29fcdebce0d9b7
medcharfi96 <1389@holbertonschool.com>
2020-05-05 18:52:42 +0200

    pep w bhema

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     tab = []
     for i in range(0, len(my_list)):
-        if i % 2 != 0:
+        if my_list[i] % 2 != 0:
             tab.append(False)
         else:
             tab.append(True)

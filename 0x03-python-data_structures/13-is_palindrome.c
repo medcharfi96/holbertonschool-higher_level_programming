98e25a7954d5310f25e13dbb0e73d230ae30a384
medcharfi96 <1389@holbertonschool.com>
2020-05-05 23:26:48 +0200

    com

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,5 +1,22 @@
 #include "lists.h"
+/**
+ * is_palindrome - check paland
+ * @head: Listint_t
+ * Return: 1(mean true)
+ */
+ 
+listint_t *get_inodex(int index, listint_t *head)
+{
+	int count = 0;
+	listint_t *test = head;
 
+	while (count < index)
+	{
+		test = test->next;
+		count++;
+	}
+	return (test);
+}	
 /**
  * is_palindrome - check paland
  * @head: Listint_t
@@ -7,34 +24,25 @@
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *test_d, *test_g, *tmp = *head;
-	int len = 0, k = 0, count;
-	test_d = *head;
-	while (tmp != NULL)
-		{
-		tmp = tmp->next;
-		len++;
-		}
-	if (len == 0)
-		return (1);
-	else if (len == 1)
-		return (0);
-	for (k = 0; k < (len / 2) ; k++)
-		{
-				while (count < (len - 1 - k))
+listint_t *test_d, *test_g, *tmp = *head;
+int len = 0, k = 0;
+test_d = *head;
+test_g = *head;
+while (tmp != NULL)
+{
+tmp = tmp->next;
+len++;
+}
+if (len == 0)
+	return (1);
+else if (len == 1)
+	return (0);
+for (k = 0; k < (len / 2) ; k++)
 	{
-		test_g = test_g->next;
-		count++;
-
-		if (!test_g)
-			return (0);
-
-	};
-	if (test_d->n != test_g->n)
-			{
+	 	test_g = get_inodex(len - 1 - k, *head);
+		if (test_d->n != test_g->n)
 				return (0);
-			}
 		test_d = test_d->next;
-		}
+	}
 	return (1);
 }

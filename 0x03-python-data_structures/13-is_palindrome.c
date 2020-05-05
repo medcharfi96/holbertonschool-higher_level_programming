4d9a3c2aa87923b58530f1c420401538f207f31b
medcharfi96 <1389@holbertonschool.com>
2020-05-05 23:15:16 +0200

    commi

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,64 +1,40 @@
 #include "lists.h"
-/**
- * is_palindrome - call the above function
- * @head: head of a list
- * return: 1 if palindome, 0 otherwise
- */
 
+/**
+ * is_palindrome - check paland
+ * @head: Listint_t
+ * Return: 1(mean true)
+ */
 int is_palindrome(listint_t **head)
 {
-	listint_t *test_g, *test_d, *tmp = *head;
-	int len = 0, k = 0, z = 0,count, res;
-	test_d = test_g = *head;
+	listint_t *test_d, *test_g, *tmp = *head;
+	int len = 0, k = 0, count;
+	test_d = *head;
 	while (tmp != NULL)
 		{
 		tmp = tmp->next;
 		len++;
 		}
 	if (len == 0)
-		res = 1;
+		return (1);
 	else if (len == 1)
-		res = 0;
-	printf("%d", len);
-	z = len;	
-	/*	if (len % 2 == 0)
+		return (0);
+	for (k = 0; k < (len / 2) ; k++)
 		{
-			while (k <= len / 2)
+				while (count < (len - 1 - k))
+	{
+		test_g = test_g->next;
+		count++;
+
+		if (!test_g)
+			return (0);
+
+	};
+	if (test_d->n != test_g->n)
 			{
-				for(count = 0; count <= z; count++)
-				{
-					test_g = test_g->next;
-				}
-				if (test_d->n == test_g->n)
-				{
-					z--;
-					k++;
-				}
-				else
-				{
-				res = 1;
-				break;
-				}
-			test_d = test_d->next;
+				return (0);
 			}
-		}*/
-		/*
-		if (i % 2 != 0)
-		{
-			while (k <= ((i - 1) / 2))
-			{
-				for(count = 0; count <= z; count++)
-				{
-					test_g = test_g->next;
-				}
-				if (test_d->n == test_g->n)
-				{
-					z--;
-					k++;
-				}
-				else
-				res = 1;
-			}
-		}*/
-	return (res);
-	}	
+		test_d = test_d->next;
+		}
+	return (1);
+}

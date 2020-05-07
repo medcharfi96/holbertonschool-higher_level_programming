cad8c8ae10620dda6b752c25899d5dbdf0ea55d3
medcharfi96 <1389@holbertonschool.com>
2020-05-07 21:45:29 +0200

    adv

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,27 @@
+#include <Python.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *print_python_list_info - show list info
+ *@p: PyObject
+ *
+ *Return: nothing(meantrue)
+ */
+
+void print_python_list_info(PyObject *p)
+{
+	PyObject *obj;
+	size_t size, i, alloc;
+	
+	size = Py_SIZE(obj);
+	alloc = ((PyListObject *)p);
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %zu\n", alloca->allocated);
+	while (i < size)
+	{
+		obj = PyList_GetItem(p, i);
+		printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name)
+		i++;
+	}
+}

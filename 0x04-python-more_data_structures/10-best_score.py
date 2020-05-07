38829fd9aac7fa9b5f2f162ea4be21c48ced275d
medcharfi96 <1389@holbertonschool.com>
2020-05-07 17:57:03 +0200

    pep w moly ya tefah

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -7,4 +7,5 @@ def best_score(a_dictionary):
         for i in a_dictionary:
             if m <= a_dictionary[i]:
                 m = a_dictionary[i]
-    return(i)
+                name = i
+    return(name)

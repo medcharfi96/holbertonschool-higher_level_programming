bbd2830bd55e02b53aa3e1f29462f53e307261a2
medcharfi96 <1389@holbertonschool.com>
2020-05-18 18:52:10 +0200

    add 0

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -6,10 +6,12 @@ def list_division(my_list_1, my_list_2, list_length):
             res = my_list_1[i] / my_list_2[i]
         except IndexError:
             print('out of range')
+            res = 0
         except TypeError:
             print('wrong type')
         except ZeroDivisionError:
             print('division by 0')
+            res = 0
         finally:
             nv_l.append(res)
     return nv_l

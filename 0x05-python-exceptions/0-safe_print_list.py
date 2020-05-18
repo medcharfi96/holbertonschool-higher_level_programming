9f7334f7c41338a5746f62abdb267396198caf4d
medcharfi96 <1389@holbertonschool.com>
2020-05-18 18:07:57 +0200

    pep

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        fn = 0
+        for i in range(x):
+            print(my_list[i], end="")
+            fn = fn + 1
+    except (IndexError):
+        pass
+    print()
+    return fn

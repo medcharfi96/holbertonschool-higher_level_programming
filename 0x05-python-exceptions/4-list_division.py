6b0d5e9a1795307c69dd3561ba9c8144747c2372
medcharfi96 <1389@holbertonschool.com>
2020-05-18 18:53:42 +0200

    add 0

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -9,6 +9,7 @@ def list_division(my_list_1, my_list_2, list_length):
             res = 0
         except TypeError:
             print('wrong type')
+            res = 0
         except ZeroDivisionError:
             print('division by 0')
             res = 0

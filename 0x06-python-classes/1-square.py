791cbcb060872d442b8d25e354e763da8762f207
medcharfi96 <1389@holbertonschool.com>
2020-05-19 17:14:22 +0200

    adddd

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+"""
+definir la classe square
+"""
+
+
 class Square:
+    """la classe square"""
     def __init__(self, size):
+        """ Example of docstring on the __init__ method
+        Args:
+            size: Description of size"""
         self.__size = size

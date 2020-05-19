dd499bd61381e1a85f989328d14f168ab1229063
medcharfi96 <1389@holbertonschool.com>
2020-05-19 17:24:25 +0200

    addd

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+definir la class Square
+"""
+
+
+class Square:
+    """la classe square"""
+    def __init__(self, size=0):
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        elif type(size) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            self.__size = size

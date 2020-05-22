c3ed72496d8af569d4bf44b845800baa9272fc26
medcharfi96 <1389@holbertonschool.com>
2020-05-22 17:16:25 +0200

    add

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -5,23 +5,22 @@ text indent° function
 
 
 def text_indentation(text):
-    """
-    Print text with two lines
-    """
-    new_string = ""
-    zab =["?",":","."]
-    bool1 = False
+    """ Print text with two lines """
+
+    zab = ["?", ":", "."]
+    test1 = False
     if type(text) is not str:
         raise TypeError('text must be a string')
-    for i in range (0,len(text)):
-        a = text[i - 1]
-        if a in zab:
-            bool1 = True
-        bool2 = text[i] == " "
-        if bool1 and bool2:
-            text[i+1].replace(text[i],"")
+    for i in range(0, len(text)):
+        prev = text[i - 1]
+        if prev in zab:
+            test1 = True
+        test2 = text[i] == " "
+        if test1 and test2:
+            print(text[i], end="")
         elif text[i] in zab:
             print(text[i])
+            print()
         else:
             print(text[i], end="")
-
+    print()

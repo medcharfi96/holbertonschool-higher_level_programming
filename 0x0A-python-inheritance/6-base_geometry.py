da0b1b6bc304dc1c6b84673b912c6fe08419775d
medcharfi96 <1389@holbertonschool.com>
2020-06-02 15:29:52 +0200

    pep

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """class descr"""
+    def area(self):
+        """raise a exception """
+        raise Exception("area() is not implemented")

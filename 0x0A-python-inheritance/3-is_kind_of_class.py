73d4a51ddc98f87c12ce70fac4e6c4d906e71688
medcharfi96 <1389@holbertonschool.com>
2020-06-02 15:10:31 +0200

    pep fix

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Function of comparing
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """check  if instance or nooooooooo """
+    if not isinstance(obj, a_class):
+        return False
+    else:
+        return True

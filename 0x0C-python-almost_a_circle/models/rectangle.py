1b53d7a6d4ced060d027b2f478177c66dedef534
medcharfi96 <1389@holbertonschool.com>
2020-06-09 16:51:55 +0200

    add

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+claass rectangle
+"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """class description"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """initialisation"""
+        super().__init__(id)
+        self.height = height
+        self.width = width
+        self.y = y
+        self.x = x

abb2ac8ac28878476db9667b8d2656ff2ecaa161
medcharfi96 <1389@holbertonschool.com>
2020-07-13 16:47:50 +0200

    add

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,9 @@
+-- requette insert 
+INSERT INTO first_table
+( -- columns to insert data into
+ [id], [name]
+)
+VALUES
+( -- first row: values for the columns in the list above
+ 89, "Holberton School"
+)

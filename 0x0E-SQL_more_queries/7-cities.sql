e85820c6e1ca14db05a8e129f7067dd632493011
medcharfi96 <1389@holbertonschool.com>
2020-07-14 15:46:48 +0200

    add

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- TASL
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+    id INT NOT NULL PRIMARY KEY UNIQUE AUTO_INCREMENT,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES  hbtn_0d_usa.states(id)
+);

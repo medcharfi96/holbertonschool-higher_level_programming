c92d62b6cb5e74522365833df9be6a49cd03213e
medcharfi96 <1389@holbertonschool.com>
2020-08-23 18:33:57 +0200

    add

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""pick find functon """
+
+
+def find_peak(list_of_integers):
+    """ last task"""
+    if len(list_of_integers) == 0:
+        return None
+    elif len(list_of_integers) == 1:
+        return (list_of_integers)    
+    for i in range(0, len(list_of_integers)-1):
+        if list_of_integers[i] >= list_of_integers[i+1]:
+            x = list_of_integers[i]
+    return (x)
+		

351056bc3cdc2b99387779797059ab0b868a7551
medcharfi96 <1389@holbertonschool.com>
2020-08-23 18:37:35 +0200

    add

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,9 +7,8 @@ def find_peak(list_of_integers):
     if len(list_of_integers) == 0:
         return None
     elif len(list_of_integers) == 1:
-        return (list_of_integers)    
+        return (list_of_integers)
     for i in range(0, len(list_of_integers)-1):
         if list_of_integers[i] >= list_of_integers[i+1]:
             x = list_of_integers[i]
     return (x)
-		

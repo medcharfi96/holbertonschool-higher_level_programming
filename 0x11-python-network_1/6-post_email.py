34e5e01d0e8b7e759c35dd41abaaa8a0ee8222e7
medcharfi96 <1389@holbertonschool.com>
2020-08-24 20:04:45 +0200

    a facillllle

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+donner lURL
+"""
+
+
+import requests
+from sys import argv
+if __name__ == "__main__":
+    req = requests.post(argv[1], data={'email': argv[2]})
+    print(req.text)

a9a0614330094a0c38c6801a76d6e68aeee296e0
medcharfi96 <1389@holbertonschool.com>
2020-09-15 15:56:01 +0200

    add

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-if (typeof(parseInt(process.argv[2])) === 'number') {
-	console.log('My number: ' + parseInt(process.argv[2]));
+if (typeof (parseInt(process.argv[2])) === 'number' && process.argv[2]) {
+  console.log('My number: ' + parseInt(process.argv[2]));
 } else {
-	console.log('Not a number');
+  console.log('Not a number');
 }

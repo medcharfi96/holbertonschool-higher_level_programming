77891c05fc2a42b4b0174c0f7acff2b5415a03aa
medcharfi96 <1389@holbertonschool.com>
2020-09-15 14:42:35 +0200

    add

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 if (process.argv.length >= 3) {
-	console.log('Argument found');
-  } else {
-	console.log('No argument');
-  }
+  console.log('Argument found');
+} else {
+  console.log('No argument');
+}

3006e4646e55d7ff95299cc43366a26a650c90f0
medcharfi96 <1389@holbertonschool.com>
2020-09-16 00:27:34 +0200

    add

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,17 +1,14 @@
 #!/usr/bin/node
-let count = 2;
+let i = 0;
 const tab = [];
-let res;
-if (process.argv.length < 4 || process.argv[3] === 1) {
-	res = 0;
+if (process.argv.length > 3) {
+  for (i = 0; i < process.argv.length; i++) {
+    if (typeof (parseInt(process.argv[i])) === 'number') {
+      tab.push(process.argv[i]);
+    }
+  }
 } else {
-	while (count < process.argv.length) {
-		if (typeof(parseInt(process.argv[count])) === 'number') {
-			tab.join(process.argv[count]);
-		}
-
-	}
+  console.log(0);
 }
-tab.sort;
-res = tab[tab.length - 1];
-console.log(res);
+tab.sort();
+console.log(tab[tab.length - 2]);

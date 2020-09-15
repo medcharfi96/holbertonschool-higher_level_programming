f581386037e9d6d15758213f60740ce38a0bc63f
medcharfi96 <1389@holbertonschool.com>
2020-09-15 23:47:00 +0200

    add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 function add (a, b) {
-	console.log(parseInt(a) + parseInt(b));
-  }
-  
-  add(parseInt(process.argv[2]), parseInt(process.argv[3]));
+  console.log(parseInt(a) + parseInt(b));
+}
+
+add(parseInt(process.argv[2]), parseInt(process.argv[3]));

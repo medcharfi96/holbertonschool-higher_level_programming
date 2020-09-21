7b0ad2ea5639763f17644344b8654c2477dad9ab
medcharfi96 <1389@holbertonschool.com>
2020-09-21 17:48:51 +0200

    add

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,34 +1,4 @@
 #!/usr/bin/node
-module.exports = class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    } else {}
-  }
-
-  print () {
-    let affichage;
-    for (let index = 0; index < this.height; index++) {
-      affichage = '';
-      for (let index1 = 0; index1 < this.width; index1++) {
-        affichage = affichage + 'X';
-      }
-      console.log(affichage);
-    }
-  }
-
-  rotate () {
-    const zabour = this.width;
-    this.width = this.height;
-    this.height = zabour;
-  }
-
-  double () {
-    this.height = this.height * 2;
-    this.width = this.width * 2;
-  }
-};
 const Rectangle = require('./4-rectangle');
 module.exports = class Square extends Rectangle {
   constructor (size) {

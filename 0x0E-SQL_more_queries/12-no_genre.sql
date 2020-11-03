6d1abcf331f1eaa987f8f18d2fd4d4c4aad14a63
medcharfi96 <1389@holbertonschool.com>
2020-11-03 15:13:01 +0100

    ad

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+-- task 12
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_show_genres LEFT JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_show_genres.show_id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;

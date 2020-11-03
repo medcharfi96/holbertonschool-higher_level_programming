7a12a2308d191c8fd370a2117f8d193292f3a6a1
medcharfi96 <1389@holbertonschool.com>
2020-11-03 15:26:40 +0100

    ad

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- Task13
 SELECT tv_genres.name AS genre, COUNT(*) AS number_shows
-FROM tv_genres RIGHT JOIN tv_show_genres
-ON tv_show_genres.genre_id = tv_genres.id 
+FROM tv_genres INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_show_genres.genre_id
 ORDER BY number_shows DESC;

605deeeb979c4cee382b4ac51c109bf95f1f48b3
medcharfi96 <1389@holbertonschool.com>
2020-11-03 15:15:48 +0100

    ad

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,7 +1,7 @@
 -- task 12
 
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows LEFT JOIN tv_shows
+FROM tv_shows LEFT JOIN tv_shows_genres
 ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_show_genres.show_id IS NULL
-ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
+ORDER BY tv_shows.title ASC;
